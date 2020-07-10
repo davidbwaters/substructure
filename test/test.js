32947a3928d425770856c4dc5e66a4fac4eec53c
David B. Waters <mrdavidbwaters@gmail.com>
2020-07-09 22:18:42 -0400

    .

--- test/test.js
+++ test/test.js
@@ -2,36 +2,41 @@
  *  Test
  */
 
+
 const fs = require('fs')
 const sass = require('sass')
 const Fiber = require('fibers')
 
+
+const build = (inPath, outPath) => {
+  
+  sass.render(
+    {
+      file: 'packages/' + inPath,
+      fiber: Fiber
+    }, 
+    (error, result) => {
+      if (!error) {
+        fs.writeFile( 
+          'test/build/' + outPath, 
+          result.css, 
+          error => {}
+        )
+      }
+      else {
+        console.log(error.formatted)
+      }
+    }
+  )
+
+}
 
 fs.mkdirSync(
   'test/build', 
   { recursive: true }
 )
 
-sass.render({
-  file: 'packages/elements-root/_index.scss',
-  outFile: 'test',
-  fiber: Fiber
-}, (error, result) => {
-  fs.writeFile( 'test/build/root.css', result.css, (error) => {} )
-})
-
-sass.render({
-  file: 'packages/utilities-sizing/_index.scss',
-  outFile: 'test',
-  fiber: Fiber
-}, (error, result) => {
-  fs.writeFile( 'test/build/sizing.css', result.css, (error) => {} )
-})
-
-sass.render({
-  file: 'packages/utilities-spacing/_index.scss',
-  outFile: 'test',
-  fiber: Fiber
-}, (error, result) => {
-  fs.writeFile( 'test/build/spacing.css', result.css, (error) => {} )
-})
+build('base-initialize/_index.scss', 'base-initialize.css')
+build('elements-root/_index.scss', 'root.css')
+build('utilities-sizing/_index.scss', 'sizing.css')
+build('utilities-spacing/_index.scss', 'spacing.css')
